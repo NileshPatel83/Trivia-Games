22936ecfb49769ec38313a273d2f891c1c267d20
TLeeming15 <tyler.leeming21@gmail.com>
2023-01-29 14:58:41 +1030

    getQuizList function add to Randomly generated ten questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,3 @@
-
-
-
-// Check for click events on the navbar burger icon to add toggleClass
-$(".navbar-burger").click(function() {
-    $(".navbar-burger").toggleClass("is-active");
-    $(".navbar-menu").toggleClass("is-active");
-});
- 
 //Ninja trivia API Key.
 const ninjaAPIKey = 'JYu2MPGTksY+oXRs5QynHQ==IvWTlwPMWz9NZLpb';
 
@@ -17,8 +8,7 @@ const triviaQueryURL = 'https://api.api-ninjas.com/v1/trivia';
 const wikipediaQueryURL = 'https://en.wikipedia.org/w/api.php';
 
 //Ninja trivia category.
-const categoryArray = ['language','sciencenature','geography','entertainment','toysgames','mathematics','religionmythology','sportsleisure'];
-
+const categoryArray = ['language','sciencenature','fooddrink','geography','entertainment','toysgames','music','mathematics','religionmythology','sportsleisure'];
 
 //Total questions to be asked.
 const totalQuestionNumber = 10;
@@ -26,190 +16,272 @@ const totalQuestionNumber = 10;
 //Total wikipedia results to display.
 let wikipediaResultNumber = 3;
 
-// Button IDs
-const startQuizButtonID = "start-quiz-button";
-
-const hintButtonID = "hint-button";
-
-const moreInfoButtonID = "more-info-button";
-
-const submitButtonID = "submit-button";
-
-const dataIndex = "data-index";
-
-// DOM elements
+//DOM Elements
 const containerEl = document.querySelector('#container');
-// Question object array : question, answer, category
-let quizQuestions;
 
-init()
+//Class names
+const submitButtonClass = 'submit-button';
 
+//ID names
+const startQuizButtonID = 'start-quiz-button';
+const hintButtonID = 'hint-button';
 
+//Attribute names
+const dataIndex = 'data-index';
+
+//Array of trivia question object which contains question, answer and category.
+let triviaQuestions;
+
+init();
+
+//Event listener for div container.
 containerEl.addEventListener('click', event => {
 
     //Gets the element that is clicked.
     let targetEl = event.target;
 
-    
-    if (targetEl.id === startQuizButtonID){
-        console.log(test);
-    
-        getQuizQuestions();
-        
-    }else if(targetEl.id === hintButtonID){
+    //If the clicked element is hint button for question, gets wikipedia search results and
+    //displays 3 results.
+    if (targetEl.id === hintButtonID){
         searchWikipedia(targetEl);
-    // wikipedia search when hint button is clicked
-    // display the wikipedia search
-    }else if (targetEl.id === moreInfoButtonID){
-
-        
-        showMoreInfo()
     }
-
+    
 });
 
-async function init (){
-    const selectedCategory = ['language', 'sciencenature','geography'];
-    quizQuestions = await getQuizQuestions(selectedCategory);
-    if (quizQuestions === "undefinded"){
+//Searches wikipedia and gets the results and displays results.
+async function searchWikipedia(hintBtnEl){
+
+    //Gets the index of hint button.
+    //This is to determine which question's hint button is clicked.
+    let index = parseInt(hintBtnEl.getAttribute(dataIndex));
+
+    //Using the index, gets the div element that contains the question elements (question, category, answer textbox and hint button).
+    let containerDivEl = containerEl.children[index];
+
+    //Gets wikipedia result for the selected question using index number.
+    let wikiSearchResults = await getWikipediaSearchResults(triviaQuestions[index]);
+    if(wikiSearchResults === 'undefined'){
         return;
     }
-    processQuizQuestions();
+
+    //Displays results in browser.
+    displayHint(containerDivEl, wikiSearchResults);
 }
 
-function processQuizQuestions(){
-    for (let i = 0; i < quizQuestions.length; i++) {
-        displayQuizQuestions(quizQuestions[i], i)
+//Displays results in browser.
+function displayHint(containerDivEl, wikiSearchResults){
+
+    //If total searches from wikipedia is less than 3, sets the length as search length.
+    if (wikiSearchResults.query.search.length < wikipediaResultNumber){
+        wikipediaResultNumber = wikiSearchResults.query.search.length;
     }
 
-    let buttonDiv = document.createElement("div");
-    let submitButton = document.createElement("button");
-    submitButton.innerHTML = "Submit";
-    buttonDiv.append(submitButton);
-    containerEl.append(buttonDiv);
+    //Creates a div element that will contain all searches.
+    let resultDivEl = document.createElement('div');
+
+    //Loop through wikipedia searches.
+    for (let i = 0; i < wikipediaResultNumber; i++) {
+
+        //Gets the seach data.
+        let searchResult = wikiSearchResults.query.search[i];
+
+        //Creates a div element that will contains wikipedia seach title and snippet.
+        let innerResultDivEl = document.createElement('div');
+
+        //Creates question as h2 element to display wikipedia search title.
+        let titleEl = document.createElement('h2');
+        titleEl.textContent = searchResult.title;
+
+        //Creates a paragraph element to display wikipedia snippet.
+        //First gets the parapgrapg inner HTML as search snippet and then
+        //Resets it using paragraph textcontent.
+        //This is to remove all HTML tags contained in search snippet received from wikipedia.
+        let paraEl = document.createElement('p');
+        paraEl.innerHTML = searchResult.snippet;
+        paraEl.innerHTML = `${paraEl.textContent}...`;
+        
+        //Adds title and snippet to inner div elememnt.
+        innerResultDivEl.append(titleEl, paraEl);
+
+        //Adds the inner div element to seach result div element.
+        resultDivEl.append(innerResultDivEl);
+    }
+
+     //Label for answer textbox.
+     let textboxLabelEl = document.createElement('label');
+     textboxLabelEl.innerHTML = 'Specific Search: ';
+
+     //Textbox for answer.
+     let answerTextboxEl = document.createElement('input');
+     answerTextboxEl.type = 'text';
+
+     let submitButtonEl = document.createElement('button');
+     submitButtonEl.innerHTML = 'More Info';
+
+    //Adds search result div element to container div.
+    containerDivEl.append(resultDivEl, textboxLabelEl, answerTextboxEl, submitButtonEl);
 }
-function displayQuizQuestions(quizQuestion, index){
-    const category = `Category: ${quizQuestion.category}`;
-    const question = `${index + 1}. ${quizQuestion.question}`;
-    const answer = quizQuestion.answer;
 
-    let questionBox = document.createElement("div");
+//Gets wikipedia result for the selected question.
+//Search result is obtained using question and asnwer both.
+//Returns the search result as an object which contains an object called 'query'.
+//'query' object contains array of all search results from wikipedia.
+async function getWikipediaSearchResults(triviaQuestion){
 
-    let innerQuestionBox = document.createElement("div");
-    
+    let params = {
+        action: "query",
+        list: "search",
+        //srsearch: `${triviaQuestion.question} + ${triviaQuestion.answer}`,
+        srsearch: triviaQuestion.answer,
+        format: "json"
+    };
 
-    let questionDisplay = document.createElement("h2");
-    questionDisplay.textContent = question;
-    
+    let requestURL = `${wikipediaQueryURL}?origin=*`;
 
-    let categoryDisplay = document.createElement("div");
-    categoryDisplay.textContent = category;
+    Object.keys(params).forEach(function(key){requestURL += "&" + key + "=" + params[key];});
 
-    let textBoxLabel = document.createElement("label");
-    textBoxLabel.setAttribute("for", answer-$(index));
-    textBoxLabel.innerHTML = "Answer:  ";
+    const response = await fetch(requestURL);
 
-    let answerTextBox = document.createElement("input");
-    answerTextBox.type = "text";
-    answerTextBox.id = `answer-${index}`;
-    answerTextBox.value = answer;
-
-    let hintButton = document.createElement("button");
-    hintButton.id = hintButtonID;
-    hintButton.setAttribute(dataIndex, index);
-    hintButton.innerHTML = "Hint";
-
-    innerQuestionBox.append(questionDisplay, categoryDisplay,textBoxLabel, answerTextBox, hintButton);
-
-    questionBox.append(innerQuestionBox);
-
-    containerEl.append(questionBox);
-
+    return response.json();
 }
 
-async function getQuizQuestions (selectedCategory) {
+//Initial function when the page is loaded.
+async function init(){
 
-    let questions= [];
+    let allQuestions = [];
+
+    //Gets list of categories selected by the user.
+    let category = ['language','sciencenature','fooddrink'];
+
+    //Loops thorugh the selected categories to get 10 questions for each category.
+    for (let i = 0; i < category.length; i++) {
+
+        //Gets the list of questions from ninja trivia for the specified category.
+        let categoryQuestions = await getTriviaQuestions(category[i]);
+        if(typeof(categoryQuestions) === 'undefined'){
+            return;
+        }
+
+        //Adds category quetions to the final list.
+        categoryQuestions.forEach(question => {
+            allQuestions.push(question);
+        });           
+    }
+
+    //Gets list of 10 questions randomly from all questions list.
+    triviaQuestions = getQuizList(allQuestions);
+    
+    //Processes trivia questions.
+    processTriviaQuestions();
+}
+
+//Processes trivia questions.
+function processTriviaQuestions(){
+    
+    //Loops through all trivial questions one by one and dispaly them in browser.
+    for (let i = 0; i < triviaQuestions.length; i++) {
+        displayTriviaQuestion(triviaQuestions[i], i)
+    }
+
+    //Creates a div a submit button.
+    let buttonDivEl = document.createElement('div');
+
+    //Creates a submit button with id and class.
+    let submitButtonEl = document.createElement('button');
+    submitButtonEl.innerHTML = 'Submit';
+    submitButtonEl.id = submitButtonClass;
+    submitButtonEl.classList.add(submitButtonClass);
+    buttonDivEl.append(submitButtonEl);
+
+    containerEl.append(buttonDivEl);
+}
+
+//Displays the question in browser using container.
+function displayTriviaQuestion(triviaQuestion, index){
+
+    const category = `Category: ${triviaQuestion.category}`;
+    const question = `${index + 1}. ${triviaQuestion.question}`;
+    const answer = triviaQuestion.answer;
+
+    //Creates a div that will contain question and wikipedia search results.
+    let questionDivEl = document.createElement('div');
+
+    //Creates a div for each question that will contain question, category, answer textbox and hint button.
+    let innerQuestionDivEl = document.createElement('div');
+    
+    //Creates question as h2 element.
+    let questionEl = document.createElement('h2');
+    questionEl.textContent = question;
+
+    //Creates a div for answer textbox and label.
+    let categoryEl = document.createElement('div');
+    categoryEl.textContent = category;
+
+    //Label for answer textbox.
+    let textboxLabelEl = document.createElement('label');
+    textboxLabelEl.setAttribute('for', `answer-${index}`);
+    textboxLabelEl.innerHTML = 'Answer: ';
+
+    //Textbox for answer.
+    let answerTextboxEl = document.createElement('input');
+    answerTextboxEl.type = 'text';
+    answerTextboxEl.id = `answer-${index}`;
+    answerTextboxEl.value = answer;
+
+    //Hint button to dispaly wikipedia search results.
+    //Creates an attribute called 'data-index'.
+    //This index value will be used to determine which question hint button is clicked.
+    let hintButtonEl = document.createElement('button');
+    hintButtonEl.id = hintButtonID;
+    hintButtonEl.setAttribute(dataIndex, index);
+    hintButtonEl.innerHTML = 'Hint';
+
+    //Adds question elements to inner div.
+    innerQuestionDivEl.append(questionEl, categoryEl, textboxLabelEl, answerTextboxEl, hintButtonEl);
+
+    //Adds inner div to outer div.
+    questionDivEl.append(innerQuestionDivEl);
+
+    //Adds outer div to main container div.
+    containerEl.append(questionDivEl);
+}
+
+//Gets list of 10 questions randomly from all questions list.
+function getQuizList(allQuestions){
+    
+    let quizList = [];
+
+    do{
+        
+        //Randomly gets the question from all questions array.
+        let randomQuiz = allQuestions[Math.floor(Math.random() * allQuestions.length)];
+        
+        //Tries to find randomly generated question from question list.
+        let existingQuestion = quizList.find(extQuiz => extQuiz.question === randomQuiz.question);
+
+        //Checks whether current question is already added to the list or not. Only adds unique questions.
+        if(typeof(existingQuestion) === 'undefined'){
+            quizList.push(randomQuiz);
+        }
+
+    }while(quizList.length < totalQuestionNumber)
+
+    return quizList;
+}
+
+//Gets the list of questions from ninja trivia.
+async function getTriviaQuestions(category){
+
+    let requestURL = `${triviaQueryURL}?category=${category}&limit=${totalQuestionNumber}`;
 
     const options = {
         method: 'GET',
         headers: { 'X-Api-Key': ninjaAPIKey},
     };
 
-    for (let i = 0; i < selectedCategory.length; i++){
-        // use API ninja to get the questions
-        let requestURL = `${triviaQueryURL}?category=${selectedCategory[i]}&limit=${totalQuestionNumber}`;
-        const response = await fetch(requestURL, options);
-        questions.push(response.json());
-    }
-
-   if (questions.length===0)return null;
-   
-   questions = generateRandomQuestions
+    const response = await fetch(requestURL, options);
 
     return response.json();
-    // display questions in the browser
-    
 }
 
-// async function searchWikipedia(hintBtnEl){
-//     let index = parseInt(hintBtnEl.getAttribute(dataIndex));
 
-    let containerDivEl = containerEl.children[index];
-
-//     //Gets wikipedia result for the selected question using index number.
-//     let wikiSearchResults = await getWikipediaSearchResults(triviaQuestions[index]);
-//     if(wikiSearchResults === 'undefined'){
-//         return;
-//     }
-//     displayHint (containerDivEl, wikiSearchResults)
-// }
-
-// function displayHint (containerDivEl, wikiSearchResults);
-
-//     if (wikiSearchResults.query.search.length < wikipediaResultNumber){
-//         wikipediaResultNumber = wikiSearchResults.query.search.length;
-
-//         let resultDivEl = document.createElement('div');
-
-//      for (let i = 0; i < wikipediaResultNumber; i++) {
-
-//         let searchResult = wikiSearchResults.query.search[i];
-// }
-
-// // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// progress bar function , for example each category has max question = 100  ---Gary
-//need to save questionComplete number in local storage to show progress
-
-// const MAX_QUESTIONS= 50;
-// function progress() {
-//     questionCorrect = 0;
-//     questionCorrect ++;
-//     progressText.innerText = `Complete ${questionComplete} of ${MAX_QUESTIONS}`;
-//     progressBarFull.style.width = `${(questionComplete/MAX_QUESTIONS) * 100}%`;
-// }
