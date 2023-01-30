a1a2e79df14d17d0bf19493650d8ab8d9c62ae4c
NileshPatel83 <hselin_83@yahoo.com>
2023-01-30 20:21:55 +1000

    updateGameScore function added to add html elements.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,12 @@
 //Local storage name
 const storageKey = 'Trivia-Games';
 
+//ID Names
+const levelDivID = 'game-level';
+const scoreDivID = 'game-score';
+
 //DOM Elements
-const magicBoxEl = document.querySelector('#magicBox');
+const magicBoxEl = document.getElementById('magicBox');
 
 init()
 
@@ -11,7 +15,34 @@ function init(){
     //Gets the local storage for trivia games.
     let gameStorage = getLocalStorage(storageKey);
 
+    updateGameScore(gameStorage);
+}
 
+function updateGameScore(gameStorage){
+
+    let scoreDivEl = document.createElement('div');
+    scoreDivEl.textContent = `Score: ${gameStorage.totalScore}`;
+    scoreDivEl.className = 'has-text-weight-bold is-size-4 mt-3';
+
+    let level = parseInt(gameStorage.level);
+
+    let levelDivOuterEl = document.createElement('div');
+    levelDivOuterEl.style.display = 'flex';
+    levelDivOuterEl.className = 'has-text-weight-bold is-size-4 mt-3';
+
+    let levelDivEl = document.createElement('div');
+    levelDivEl.textContent = 'Level:';
+    levelDivEl.style.lineHeight = '50px';
+
+    let imageEl = document.createElement('img');
+    imageEl.src = 'assets/images/coin.png';
+    //imageEl.setAttribute('height', '50px');
+    imageEl.style.height = '50px';
+
+    levelDivOuterEl.append(levelDivEl,imageEl);
+
+
+    magicBoxEl.append(scoreDivEl, levelDivOuterEl);
 }
 
 //Gets the local storage for trivia games.
